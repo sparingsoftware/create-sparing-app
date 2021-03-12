640c8b283da875a6b1f4a623fec7993afe29e80b
Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>
2021-03-12 16:04:29 +0100

    Fix cli execution

--- dist/modules/show-final-info.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const utils_1 = require("../utils");
-function showFinalInfo() {
-    utils_1.log.info('Done');
-}
-exports.default = showFinalInfo;
--- dist/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TemplateName = void 0;
-var TemplateName;
-(function (TemplateName) {
-    TemplateName["Nuxt"] = "nuxt";
-    TemplateName["Simple"] = "simple";
-})(TemplateName = exports.TemplateName || (exports.TemplateName = {}));
