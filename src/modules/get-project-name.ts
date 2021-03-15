6a7d4f2516fcf8238f14f03a8f3d19beb0f3bf61
Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>
2021-03-15 15:47:06 +0100

    Add checking file existence in project creation

--- src/modules/get-project-name.ts
+++ src/modules/get-project-name.ts
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import inquirer from 'inquirer'
 import validate from 'validate-npm-package-name'
 import { InputQuestion } from 'inquirer'
@@ -7,9 +8,17 @@ export default async function getProjectName() {
     name: 'projectName',
     type: 'input',
     message: 'Project name:',
-    validate: (input: string) =>
-      validate(input).validForNewPackages ||
-      'Project name must be valid npm package name'
+    validate: (input: string) => {
+      if (!validate(input).validForNewPackages) {
+        return 'Project name must be valid npm package name!'
+      }
+
+      if (fs.existsSync(input)) {
+        return `Project '${input}' already exists in current directory!`
+      }
+
+      return true
+    }
   }
 
   const answers = await inquirer.prompt<{ projectName: string }>([
