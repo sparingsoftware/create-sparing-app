b15c0e1dae76ae7c9546049f32ccd4d9f858b78a
Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>
2021-03-15 14:16:15 +0100

    Rename scss to sass

--- templates/nuxt/nuxt.config.ts
+++ templates/nuxt/nuxt.config.ts
@@ -22,7 +22,7 @@ const config: NuxtConfig = {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['~/assets/scss/main.scss'],
+  css: ['~/assets/sass/main.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
